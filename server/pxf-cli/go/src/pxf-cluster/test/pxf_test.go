b0d9314e95e124a8933b86fca34eef10ab8c6f5d
Oliver Albertini <oliverralbertini@users.noreply.github.com>
2018-11-13 16:41:26 -0800

    Fix the go dependencies in the pxf base docker image (#45)
    * Cleanup go dependencies in the dockerfile
    * Add go dependencies before building pxf in the dockerfile
    * Separate out pxf-cli make commands from java
    * Fix broken tests due to pxf-cli command output change
    
    Co-authored-by: Oliver Albertini <oalbertini@pivotal.io>
    Co-authored-by: Lav Jain <ljain@pivotal.io>

--- server/pxf-cli/go/src/pxf-cluster/test/pxf_test.go
+++ server/pxf-cli/go/src/pxf-cluster/test/pxf_test.go
@@ -15,6 +15,7 @@ var _ = Describe("MakeValidCliInputs", func() {
 	BeforeEach(func() {
 		oldGphome, isGphomeSet = os.LookupEnv("GPHOME")
 		os.Setenv("GPHOME", "/test/gphome")
+		os.Setenv("PXF_CONF", "/test/gphome/pxf_conf")
 	})
 
 	AfterEach(func() {
@@ -31,21 +32,29 @@ var _ = Describe("MakeValidCliInputs", func() {
 		Expect(err).To(BeNil())
 		Expect(inputs).To(Equal(&pxf.CliInputs{
 			Gphome: "/test/gphome",
+			PxfConf: "/test/gphome/pxf_conf",
 			Args:   []string{"init"},
 		}))
+	})
+
+	It("Init fails when PXF_CONF is not set", func() {
+		os.Unsetenv("PXF_CONF")
+		inputs, err := pxf.MakeValidCliInputs(validArgs)
+		Expect(err).To(Equal(errors.New("PXF_CONF must be set.")))
+		Expect(inputs).To(BeNil())
 	})
 
 	It("Fails when GPHOME is not set", func() {
 		os.Unsetenv("GPHOME")
 		inputs, err := pxf.MakeValidCliInputs(validArgs)
-		Expect(err).To(Equal(errors.New("GPHOME is not set")))
+		Expect(err).To(Equal(errors.New("GPHOME must be set.")))
 		Expect(inputs).To(BeNil())
 	})
 
 	It("Fails when GPHOME is blank", func() {
 		os.Setenv("GPHOME", "")
 		inputs, err := pxf.MakeValidCliInputs(validArgs)
-		Expect(err).To(Equal(errors.New("GPHOME is blank")))
+		Expect(err).To(Equal(errors.New("GPHOME cannot be blank.")))
 		Expect(inputs).To(BeNil())
 	})
 
@@ -62,13 +71,13 @@ var _ = Describe("MakeValidCliInputs", func() {
 	})
 
 	It("Fails when extra arguments are passed", func() {
-		inputs, err := pxf.MakeValidCliInputs([]string {"pxf-cluster", "init", "abc"})
+		inputs, err := pxf.MakeValidCliInputs([]string{"pxf-cluster", "init", "abc"})
 		Expect(err).To(Equal(errors.New("usage: pxf cluster {start|stop|restart|init|status}")))
 		Expect(inputs).To(BeNil())
 	})
 
 	It("Fails when the subcommand is not valid", func() {
-		inputs, err := pxf.MakeValidCliInputs([]string {"pxf-cluster", "invalid"})
+		inputs, err := pxf.MakeValidCliInputs([]string{"pxf-cluster", "invalid"})
 		Expect(err).To(Equal(errors.New("usage: pxf cluster {start|stop|restart|init|status}")))
 		Expect(inputs).To(BeNil())
 	})
@@ -78,7 +87,7 @@ var _ = Describe("RemoteCommandToRunOnSegments", func() {
 	It("constructs a list of shell args from the input", func() {
 		inputs := &pxf.CliInputs{
 			Gphome: "/test/gphome",
-			Args: []string{"init"},
+			Args:   []string{"init"},
 		}
 		expected := []string{"/test/gphome/pxf/bin/pxf", "init"}
 
