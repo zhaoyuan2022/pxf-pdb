ab0fbbac13f26551d995bca109aa98cd149a6f33
Lav Jain <ljain@pivotal.io>
2019-03-28 11:28:20 -0700

    Remove gpaddon from PXF PR pipeline (#122)
        * Updated PXF PR pipeline to avoid using gpaddon
        * compile and install gpdb from source
        * Removed references to secrets
    
        Authored-by: Shivram Mani <smani@pivotal.io>
        Authored-by: Lav Jain <ljain@pivotal.io>

--- concourse/scripts/pxf_common.bash
+++ concourse/scripts/pxf_common.bash
@@ -42,11 +42,28 @@ function run_regression_test() {
 	su gpadmin -c "bash /home/gpadmin/run_regression_test.sh $(pwd)"
 }
 
+function build_install_gpdb() {
+    pushd gpdb_src
+        CC=$(which gcc) CXX=$(which g++) ./configure \
+        --with-perl \
+        --with-python \
+        --with-libxml \
+        --with-zstd \
+        --disable-orca \
+        --disable-gpfdist \
+        --prefix=${GPHOME}
+        make -j4 -s
+        make -s install
+    popd
+
+}
 function install_gpdb_binary() {
 
-	if [[ ! -d ${GPHOME}/bin/psql ]]; then
+	if [[ -d bin_gpdb ]]; then
 		mkdir -p ${GPHOME}
 		tar -xzf bin_gpdb/*.tar.gz -C ${GPHOME}
+	else
+	    build_install_gpdb
 	fi
 
 	if [[ ${TARGET_OS} == "centos" ]]; then
