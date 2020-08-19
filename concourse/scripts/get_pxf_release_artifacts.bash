9f5a0bfeb6a51af6c5983079b1fe89e0f97c83c6
Alexander Denissov <denalex@users.noreply.github.com>
2020-08-19 15:06:17 -0700

    build pipeline last step to process OSL files and send email (#424)
    * added generate releng email task
    
    * updated execution mode
    
    * adjusted message
    
    * remove path from artifacts
    
    * adjusted text
    
    * update get PXF artifacts job
    
    * fix script
    
    * parameterized RelEng drop points
    
    * remove obsolete line
    
    * print out email
    
    * added extra line
    
    * updated build pipeline template for OSL/ODP processing and email sending

--- concourse/scripts/get_pxf_release_artifacts.bash
+++ concourse/scripts/get_pxf_release_artifacts.bash
@@ -4,22 +4,53 @@ set -e
 
 : "${GCS_RELEASES_BUCKET:?GCS_RELEASES_BUCKET must be set}"
 : "${GCS_RELEASES_PATH:?GCS_RELEASES_PATH must be set}"
+: "${GCS_OSL_PATH:?GCS_OSL_PATH must be set}"
+: "${GCS_ODP_PATH:?GCS_ODP_PATH must be set}"
 : "${GOOGLE_CREDENTIALS:?GOOGLE_CREDENTIALS must be set}"
 
-copy_to_artifacts_dir() {
-  local source=gs://${GCS_RELEASES_BUCKET}/${GCS_RELEASES_PATH}/gp${1}/${2}
-  local destination=pxf_artifacts
-  if gsutil ls "${source}" >/dev/null 2>&1; then
-    echo "Copying ${source} to ${destination}..."
-    gsutil cp "${source}" "${destination}"
-  fi
+
+function fail() {
+  echo "Error: $1"
+  exit 1
 }
 
+check_artifacts() {
+  for artifact in "${artifacts[@]}"; do
+    gsutil ls "gs://${GCS_RELEASES_BUCKET}/${artifact}" >/dev/null 2>&1 || fail "Expected artifact not found: ${artifact}"
+  done
+}
+
+copy_artifacts_to_local() {
+  for artifact in "${artifacts[@]}"; do
+    local source="gs://${GCS_RELEASES_BUCKET}/${artifact}"
+    echo "Copying ${source} to ${destination_dir} ..."
+    gsutil cp "${source}" "${destination_dir}"
+  done
+}
+
+# authenticate to Google
 echo "Authenticating with Google service account..."
 gcloud auth activate-service-account --key-file=<(echo "${GOOGLE_CREDENTIALS}") >/dev/null 2>&1
 
-version=$(< pxf_open_source_license_file/version)
+# determine PXF version to ship
+[[ -f pxf_shipit_file/version ]] || fail "Expected shipit file not found"
+version=$(<pxf_shipit_file/version)
 
-copy_to_artifacts_dir 5 "pxf-gp5-${version}-1.el6.x86_64.rpm"
-copy_to_artifacts_dir 5 "pxf-gp5-${version}-1.el7.x86_64.rpm"
-copy_to_artifacts_dir 6 "pxf-gp6-${version}-1.el7.x86_64.rpm"
+echo "Ship directive for PXF-${version} from : $(<pxf_shipit_file/*.txt)"
+
+# define artifacts to copy
+artifacts=(
+  "${GCS_RELEASES_PATH}/gp5/pxf-gp5-${version}-1.el6.x86_64.rpm"
+  "${GCS_RELEASES_PATH}/gp5/pxf-gp5-${version}-1.el7.x86_64.rpm"
+  "${GCS_RELEASES_PATH}/gp6/pxf-gp6-${version}-1.el7.x86_64.rpm"
+  "${GCS_OSL_PATH}/open_source_license_VMware_Tanzu_Greenplum_Platform_Extension_Framework_${version}_GA.txt"
+  "${GCS_ODP_PATH}/VMware-greenplum-pxf-${version}-ODP.tar.gz"
+)
+
+# check and copy artifacts to local destination
+destination_dir=pxf_artifacts
+check_artifacts
+copy_artifacts_to_local
+
+echo "Contents of the destination directory ${destination_dir}:"
+ls -al ${destination_dir}
